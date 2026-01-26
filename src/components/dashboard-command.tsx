2dd9fe8bf044f02cc9ba84f4477f1479a3ec6604
aabhaysharmaa <abhaysharma9667200@gmail.com>
2026-01-26 06:42:49 +0530

    feature : responsive dialog

--- src/components/dashboard-command.tsx
+++ src/components/dashboard-command.tsx
@@ -1,5 +1,5 @@
 import { Dispatch, SetStateAction } from "react";
-import { CommandDialog, CommandInput, CommandItem, CommandList } from "./ui/command"
+import { CommandInput, CommandItem, CommandList, CommandResponsiveDialog } from "./ui/command";
 
 interface Props {
 	open: boolean;
@@ -9,13 +9,13 @@ interface Props {
 
 export const DashboardCommand = ({ open, setOpen }: Props) => {
 	return (
-		<CommandDialog open={open} onOpenChange={setOpen}>
+		<CommandResponsiveDialog open={open} onOpenChange={setOpen}>
 			<CommandInput placeholder="Find a meeting or agent" />
 			<CommandList>
 				<CommandItem>
 					Test
 				</CommandItem>
 			</CommandList>
-		</CommandDialog>
+		</CommandResponsiveDialog>
 	)
 }
